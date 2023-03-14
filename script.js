63761dc56598e52157a6b4073bff3a4ad3c73aa0
vickychandra357 <81799094+vickychandra357@users.noreply.github.com>
2023-03-14 20:28:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+ const myPromise=new Promise((resolve,reject)=>{
+	 setTimeout(()=>{
+		 resolve("Hello,World!");
+	 },1000)
+ });
+document.getElementById("output").innerHtml=myPromise;
